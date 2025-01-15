871f801f1d00f6ca2a8d2a07a30f76204b567919
Ahkla <ahkla185@hotmail.com>
2025-01-15 18:36:42 -0500

    se agrego la inferaz del consumo junto con la funcion para formatear la moneda

--- src/components/OrderItemsComponent.tsx
+++ src/components/OrderItemsComponent.tsx
@@ -1,5 +1,5 @@
 import type { OrderItem } from "../types"
-
+import { formatCurrency } from "../helpers"
 type OrderItemProps = {
     order : OrderItem[]
 }
@@ -8,13 +8,24 @@ export default function OrderItemsComponent({order}:OrderItemProps) {
   return (
     <div>
         <h1 className="font-black text-4xl">Consumo</h1>
-        <div className="space-y-5 mt-5">
+        <div className="space-y-5 mt-10">
           {order.length === 0 
-          ? <p>No hay ordenes</p>
+          ? <p className="text-center">No hay ordenes</p>
           : (
               order.map(item => (
-                <div key={item.id}>
-                  <p>{item.name}</p>
+                <div 
+                key={item.id}
+                className="flex justify-between border-t border-gray-200 p-5 last-of-type:border-b items-center"
+                >
+                  <div>
+                    <p>{item.name} - {formatCurrency(item.price)}</p>
+                    <p className="font-black ">Cantidad: {item.quantity} - {formatCurrency(item.quantity * item.price)} </p>
+                  </div>
+                  <button 
+                  className="bg-red-600 text-white rounded-full h-8 w-8"
+                  >
+                    X
+                  </button>
                 </div>
               ))
           )}
