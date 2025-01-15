62edd9454550cef91b89bac0e28eae10498096ba
Ahkla <ahkla185@hotmail.com>
2025-01-15 16:34:42 -0500

    Se organizo la funcion de addOrder, validandando si el elemento ya esta agregado para aumentarle la quantity, en caso de no estar agregado se agrega

--- src/hooks/useOrder.tsx
+++ src/hooks/useOrder.tsx
@@ -4,8 +4,18 @@ export default function useOrder(){
     const[order,setOrder] = useState<OrderItem[]>([])
 
     const addOrder = (item: MenuItem) => {
-        console.log(item);
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if (itemExist){
+            const updateItem = order.map(orderItem => orderItem.id === item.id ? {...orderItem, quantity: orderItem.quantity+1 } : orderItem)
+            setOrder(updateItem)
+        }else{
+            const newItem = {...item,quantity : 1}
+            setOrder([...order,newItem])
+        }
+     
     }
+    console.log(order);
+    
     return {
         addOrder
     }
